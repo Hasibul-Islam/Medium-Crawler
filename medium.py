febae6a3bfe294e2874e35d1e873715eaf880f39
unknown <polok.hasibul@gmail.com>
2020-12-15 22:19:08 +0600

    3rd attempt

--- medium.py
+++ medium.py
@@ -56,107 +56,109 @@ while try_count>0:
 topic_urls = page_source.find_all('a',{'class':'u-flex0 u-height180 u-backgroundCover'})
 topic_urls = [url['href'] for url in topic_urls]
 
-print(len(topic_urls))
+if len(topic_urls)==0:
+	print('Could not load the page!')
+else:
 
-time.sleep(10)
+	time.sleep(10)
 
-for url in topic_urls[1:2]:
-	try_count=3
-	while try_count>0:
-		try:
-			with webdriver.Chrome(executable_path=chrome_driver_path,options=chrome_options) as driver:
-				driver.set_page_load_timeout(3000)
-				# action = ActionChains(driver)
-				driver.get(url)
-				elem = driver.find_element_by_tag_name('body')
-				no_of_pagedowns = 3
-				while no_of_pagedowns:
-					elem.send_keys(Keys.PAGE_DOWN)
-					time.sleep(3)
-					no_of_pagedowns-=1
-					print(no_of_pagedowns)
+	for url in topic_urls[1:2]:
+		try_count=3
+		while try_count>0:
+			try:
+				with webdriver.Chrome(executable_path=chrome_driver_path,options=chrome_options) as driver:
+					driver.set_page_load_timeout(3000)
+					# action = ActionChains(driver)
+					driver.get(url)
+					elem = driver.find_element_by_tag_name('body')
+					no_of_pagedowns = 15
+					while no_of_pagedowns:
+						elem.send_keys(Keys.PAGE_DOWN)
+						time.sleep(3)
+						no_of_pagedowns-=1
+						print(no_of_pagedowns)
 
 
-				html_to_text = BeautifulSoup(driver.page_source,features="html.parser")
-				driver.close()
-				break
-		except:
-			print('trying again to fetch urls')
-		try_count-=1
+					html_to_text = BeautifulSoup(driver.page_source,features="html.parser")
+					driver.close()
+					break
+			except:
+				print('trying again to fetch urls')
+			try_count-=1
 
-array_of_articles = html_to_text.find_all('h3',{'class':'bj gb gh bl aw eo ep at eq av er gm aq'})
+	array_of_articles = html_to_text.find_all('h3',{'class':'bj gb gh bl aw eo ep at eq av er gm aq'})
 
-array_of_articles = [h3.find('a')['href'] for h3 in array_of_articles]
-print(len(array_of_articles))
+	array_of_articles = [h3.find('a')['href'] for h3 in array_of_articles]
+	print(len(array_of_articles))
 
-time.sleep(20)
-for article_link in array_of_articles:
-
-	try_count=3
-	while try_count>0:
-		try:
-			if article_link.startswith('https'):
-				link = article_link.split('?')[0]
-			else:
-				link ='https://medium.com'+ article_link.split('?')[0]
-			with webdriver.Chrome(executable_path=chrome_driver_path,options=chrome_options) as driver:
-				driver.set_page_load_timeout(3000)
-				driver.get(link)
-				html_to_text = BeautifulSoup(driver.page_source,features="html.parser")
-				try:
-					blog_title = html_to_text.find('h1').text.strip()
-				except:
-					blog_title = None
-				blog_topic = ""
-				print(blog_title)
-				paragraphs = html_to_text.find_all('p')
-				
-				article_content = [para.text.strip() for para in paragraphs]
-				# print(article_content)
-				# iamages = html_to_text.find_all('img')
-				# for img in iamages:
-				# 	print(img['src'])
-				# header_image = ''
-				# try:
-				# 	blog_author = html_to_text.find('a',{'class':'du gm bc bd be bf bg bh bi bj gn bm go gp'}).text.strip()
-				# except:
-				# 	blog_author = None
-				# print(blog_author)
-				# try:
-				# 	published_at = html_to_text.find('a',{'class':'du gm bc bd be bf bg bh bi bj gn bm go gp'}).text.strip()
-				# 	# published_at = datetime.strptime(published_at.split('T')[0], '%Y-%m-%d')
-				# 	print(published_at)
-				# except:
-				# 	published_at = None
-				# if not published_at:
-				# 	print(link)
-				# block_para = main_content.find_all('blockquote')
-				# blockquotes = [block.text.strip() for block in block_para]
-				# article = {
-				# 	'title':blog_title,
-				# 	'topic': blog_topic,
-				# 	'author_name':blog_author,
-				# 	'body':article_content,
-				# 	'summary':article_content[:5],
-				# 	'published_at':published_at,
-				# 	'images_url':header_image,
-				# 	'blockquote':blockquotes,
-				# 	'heart':[],
-				# 	'heart_count': 0,
-				# 	'view_count': 0,
-				# 	'comment':[],
-				# 	'fetch_from_internet':True,
-				# 	'domain': 'huffpost',
-				# 	'article_url': link,
-
-				# }
-				
-
-				# print(article)
-				driver.close()
-				break
-		except:
-			print("trying again to fetch content!")
-			time.sleep(20)
-		try_count-=1
 	time.sleep(20)
+	for article_link in array_of_articles:
+
+		try_count=3
+		while try_count>0:
+			try:
+				if article_link.startswith('https'):
+					link = article_link.split('?')[0]
+				else:
+					link ='https://medium.com'+ article_link.split('?')[0]
+				with webdriver.Chrome(executable_path=chrome_driver_path,options=chrome_options) as driver:
+					driver.set_page_load_timeout(3000)
+					driver.get(link)
+					html_to_text = BeautifulSoup(driver.page_source,features="html.parser")
+					try:
+						blog_title = html_to_text.find('h1').text.strip()
+					except:
+						blog_title = None
+					blog_topic = ""
+					print(blog_title)
+					paragraphs = html_to_text.find_all('p')
+					
+					article_content = [para.text.strip() for para in paragraphs]
+					# print(article_content)
+					# iamages = html_to_text.find_all('img')
+					# for img in iamages:
+					# 	print(img['src'])
+					# header_image = ''
+					# try:
+					# 	blog_author = html_to_text.find('a',{'class':'du gm bc bd be bf bg bh bi bj gn bm go gp'}).text.strip()
+					# except:
+					# 	blog_author = None
+					# print(blog_author)
+					# try:
+					# 	published_at = html_to_text.find('a',{'class':'du gm bc bd be bf bg bh bi bj gn bm go gp'}).text.strip()
+					# 	# published_at = datetime.strptime(published_at.split('T')[0], '%Y-%m-%d')
+					# 	print(published_at)
+					# except:
+					# 	published_at = None
+					# if not published_at:
+					# 	print(link)
+					# block_para = main_content.find_all('blockquote')
+					# blockquotes = [block.text.strip() for block in block_para]
+					# article = {
+					# 	'title':blog_title,
+					# 	'topic': blog_topic,
+					# 	'author_name':blog_author,
+					# 	'body':article_content,
+					# 	'summary':article_content[:5],
+					# 	'published_at':published_at,
+					# 	'images_url':header_image,
+					# 	'blockquote':blockquotes,
+					# 	'heart':[],
+					# 	'heart_count': 0,
+					# 	'view_count': 0,
+					# 	'comment':[],
+					# 	'fetch_from_internet':True,
+					# 	'domain': 'huffpost',
+					# 	'article_url': link,
+
+					# }
+					
+
+					# print(article)
+					driver.close()
+					break
+			except:
+				print("trying again to fetch content!")
+				time.sleep(20)
+			try_count-=1
+		time.sleep(20)
